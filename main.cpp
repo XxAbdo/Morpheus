2ecc3f177ac1d39895207fb772299c868b2df5c2
XxAbdo <XxAbdo@ABDO>
2025-12-15 15:02:19 +0300

    Added the constant time matrix and addition/subtraction of matrices

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 int main(){
 
 	Matrix m1({{1,3},{2,4}});
+	Matrix m3({ {1,3},{2,4} });
+
 	Matrix m2({{5,7,-1},{0,5,6}},std::make_tuple(2,3));
-	Matrix product = Matrix::dot(m1,m2);
+	Matrix product = Matrix::Addmatrix(m1,m3);
 	product.print();
 	std::cout << "\n";
 
